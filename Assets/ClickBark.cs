09013561e3d55d36931e3add76654714b3e2f3c9
marvin1314 <93797659+marvin1314@users.noreply.github.com>
2022-11-10 18:56:29 -0500

    Bark improvements

--- Assets/ClickBark.cs
+++ Assets/ClickBark.cs
@@ -8,9 +8,11 @@ public class ClickBark : MonoBehaviour
     public GameObject BarkText;
     public string text;
     public int thres = 40;
+    private static float t;// = .05f;
     // Start is called before the first frame update
     void Start()
     {
+        //t = 0f;
         BarkText.SetActive(false);
     }
     Vector3 worldPosition;
@@ -23,9 +25,18 @@ public class ClickBark : MonoBehaviour
         //Debug.Log((transform.position - worldPosition).magnitude);
         if (Input.GetMouseButtonDown(0) && Mathf.Abs((transform.position - worldPosition).magnitude) < thres)
         {
+            Debug.Log(Time.time);
+            t = Time.time + 1.5f;
+            Debug.Log("c:" + Time.time + "e:" + t);
             BarkText.GetComponent<TextMeshProUGUI>().SetText(text);
             BarkText.SetActive(!BarkText.activeSelf);
-            Debug.Log("Hehehe");
+            Debug.Log("Hehehe"+ (Time.time >= t));
+        }
+        if (Time.time >= t && BarkText.activeSelf)
+        {
+            Debug.Log("c:" + Time.time + " e:" + t);
+            Debug.Log("AAAHAHLSA");
+            BarkText.SetActive(false);
         }
     }
 }
