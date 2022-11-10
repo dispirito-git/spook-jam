eff3af3253ba835a73a914991a3e4cb6b744120b
marvin1314 <93797659+marvin1314@users.noreply.github.com>
2022-11-10 00:03:12 -0500

    Improved Poem Notes

--- Assets/Note.cs
+++ Assets/Note.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Note : MonoBehaviour, IPointerDownHandler
+public class Note : MonoBehaviour
 {
     public GameObject noteBox;
+    GameObject noteBox1;
     public string text;
+    public string text1;
     // Start is called before the first frame update
     void Start()
     {
+        noteBox1 = noteBox.GetComponentsInChildren<Image>()[1].gameObject;
         noteBox.SetActive(false);
+        //noteBox1.SetActive(false);
+        //Following reformats the text input if needed to be on two lines
+        int dex = text.IndexOf('~');
+        if (dex!= -1)
+        {
+            text = text.Substring(0, dex) + "\n" + text.Substring(dex + 1);
+        }
     }
     Vector3 worldPosition;
     int thres = 40;
@@ -24,13 +35,19 @@ public class Note : MonoBehaviour, IPointerDownHandler
         if (Input.GetMouseButtonDown(0) && Mathf.Abs((transform.position-worldPosition).magnitude)< thres)
         {
             noteBox.GetComponentInChildren<TextMeshProUGUI>().SetText(text);
-            noteBox.SetActive(!noteBox.activeSelf);
+            if(text1.Length>1)
+            {
+                noteBox1.GetComponentInChildren<TextMeshProUGUI>().SetText(text1);
+                noteBox.SetActive(!noteBox.activeSelf);
+            }
+            else
+            {
+                noteBox.SetActive(!noteBox.activeSelf);
+                noteBox1.SetActive(!noteBox.activeSelf);
+            }
+            
             Debug.Log("AAAAAA");
         }
-    }
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        Debug.Log("OnPointerDown");
     }
    
 }
